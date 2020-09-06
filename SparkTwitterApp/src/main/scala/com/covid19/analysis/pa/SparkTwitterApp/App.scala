a5997ffa2fb2c16ea643bdede5dc685d1e4bb7e9
ksanchez <kenrysanchez65@gmail.com>
2020-09-06 14:51:36 -0500

    fixing positions to specific input and output directories

--- SparkTwitterApp/src/main/scala/com/covid19/analysis/pa/SparkTwitterApp/App.scala
+++ SparkTwitterApp/src/main/scala/com/covid19/analysis/pa/SparkTwitterApp/App.scala
@@ -29,9 +29,6 @@ object App {
   /**
    * PRIVATE ATTRIBUTES
    */
-
-  //TODO: Try to optimize it later to work in docker
-  val FILE_PATH = "/Users/kenrysanchez/DEV/PA-Covid-19-Analysis/resources/tweets/demo/demo_tweet.json"
   
   val spark = SparkSession.builder().master("local").appName("Twitter_Spark_App").getOrCreate()
   val sparkContext = spark.sparkContext
@@ -43,7 +40,7 @@ object App {
 
   def main(args: Array[String]) {
         
-    val jsonFile = sparkContext.textFile(FILE_PATH).reduce(_ + _)
+    val jsonFile = sparkContext.textFile(args(0)).reduce(_ + _)
     val delimitedFile = jsonFile.replaceAll(" ", "").replaceAll("\\}\\{", "\\}\\##\\{")
     
     val rdd = sparkContext.parallelize(List(delimitedFile))
@@ -77,7 +74,7 @@ object App {
       csvFile.writeAll(list.toList.asJava)
       Iterator(stringWritter.toString)
 
-    }).saveAsTextFile(args(0))
+    }).saveAsTextFile(args(1))
 
   }
 
