a906b66fb75bb0ba1aa50ca7c81a45842064d4f5
ksanchez <kenrysanchez65@gmail.com>
2020-09-18 01:51:35 -0500

    initializing spark testing driver

---
+++ SparkTwitterApp/src/test/scala/com/covid19/analysis/pa/test/AppTesting.scala
@@ -0,0 +1,22 @@
+
+
+package com.covid19.analysis.pa.test
+
+import org.apache.spark.SharedSparkContext
+import org.scalatest.FunSuite
+
+/**
+ * @author KASV
+ */
+
+class AppTesting extends FunSuite with SharedSparkContext {
+  
+  test("testingBuildingJsonSchema") {
+    
+  }
+  
+  test("testingJsonParser") {
+    
+  }
+
+}
